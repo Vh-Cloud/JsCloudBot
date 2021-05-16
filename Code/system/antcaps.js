af1910c3bdf50b31cee9376a15b77fd21beae976
Victor Henrique Ventura Freitas <79233369+VhCompany1@users.noreply.github.com>
2021-05-16 06:20:27 -0300

    Update antcaps.js

--- Code/system/antcaps.js
+++ Code/system/antcaps.js
@@ -1,8 +1,26 @@
-module.exports.run = async (client, message, args) => {
-    if (message.author.id == 842899922160058388) return;
+module.exports.run = async (client, message, args) => {    
+    if (message.author.bot) return;
 
-    if(message.content == message.content.toUpperCase() && message.content.length != 0 && message.content.length != 22){
+    const length = message.content.length
+    let account = 0
+
+    for(let c = 1;c <= length - 1;c++){
+        if(message.content[c] == message.content[c].toUpperCase()&&message.content[c].toUpperCase() != "K"){
+            account++
+        }else{}
+    }
+
+    if(account >= length / 2){
         message.delete().catch(O_o => {})
         return message.reply(`Nos não gostamos de capslock dentro de nossos servidores`)
     }
+
+    console.log(`Caps detected`)
+
+    /*if(message.content == message.content.toUpperCase() && message.content.length != 0 && message.content.length != 22){
+        simplecaps = true
+        
+        message.delete().catch(O_o => {})
+        return message.reply(`Nos não gostamos de capslock dentro de nossos servidores`)
+    }*/
 }
