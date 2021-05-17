411b170201e07da7fc4b38c52e9b05f2866e89cb
Victor Henrique Ventura Freitas <79233369+VhCompany1@users.noreply.github.com>
2021-05-17 18:37:40 -0300

    1.2.0

--- Code/commands/botinfo.js
+++ Code/commands/botinfo.js
@@ -1,8 +1,17 @@
 Discord = require("discord.js");
+
+const config = require("../config.json")
 const { prefix } = require("../config.json")
 
+const command = "clear"
+
+/*----------------------------------------------------*/
+
 const c = '`'
-const botVersion = 'BETA 1.8.2'
+const { botversion } = require("../config.json")
+
+/*----------------------------------------------------*/
+
 module.exports.run = async (client, message, args) => {
   let totalSeconds = client.uptime / 1000;
   let days = Math.floor(totalSeconds / 86400);
@@ -11,25 +20,25 @@ module.exports.run = async (client, message, args) => {
   let minutes = Math.floor(totalSeconds / 60);
   let seconds = totalSeconds % 60;
 
-  const HelpEmbed = new Discord.MessageEmbed()
-    .setColor("#7FFFD4")
-    .setTitle(`InfoBot`)
+  const Embed = new Discord.MessageEmbed()
+    .setColor("#6272a4")
+    .setTitle(`Botinfo`)
     .setDescription(`
       **<:dev:771577780369621003> Infornações detalhadas**
 
-      **<a:Desenvolvedor:814624241277599764> Desenvolvedor:**  ${c}VhCompany#9999${c}
+      **<a:Desenvolvedor:814624241277599764> Desenvolvedor:**  [VhCompany](https://github.com/VhCompany1)
 
-      **<:vscode:810701033079963648> Empresa:** ${c}VhCloud${c}
+      **<:vscode:810701033079963648> Empresa:** [VhCloud](https://github.com/vh-cloud)
 
-      **🤖 Meu prefixo:**  ${c}#${c} 
+      **🤖 Meu prefixo:**  ${c}${prefix}${c} 
 
-      **<:discord_logo:799345418126884914> Bot Version:**  ${c}${botVersion}${c}
+      **<:discord_logo:799345418126884914> Bot Version:**  ${c}${botversion}${c}
 
       **💻 Host:**  ${c}Repl.it${c}
 
-      **<a:a_BlueVerificMark:782393699786424350> Data de verificação:** ${c}31/03/2021${c}
+      **<a:a_BlueVerificMark:782393699786424350> Data de verificação:** ${c}?${c}
 
-      **<:flechabaixo:805212916969373696> Data de criação:** ${c}13/02/2021${c}
+      **<:flechabaixo:805212916969373696> Data de criação:** ${c}15/05/2021${c}
 
       **<:JavaSkript:810275989451309056> Linguagem:** ${c}JavaScript${c}
 
@@ -37,7 +46,7 @@ module.exports.run = async (client, message, args) => {
 
       **<:database:815991106461630495> DataBase:** ${c}LowDb${c}
 
-      **<:bot:814883289651085344> Comandos:** ${c}155${c}
+      **<:bot:814883289651085344> Comandos:** ${c}7${c}
 
       **<:linguaguem:812125359704899596> Ver todos os comandos:** ${c}${prefix}help${c} 
 
@@ -49,7 +58,7 @@ module.exports.run = async (client, message, args) => {
       `)
       .setImage(`https://www.seekpng.com/png/detail/978-9786743_anime-popular-girl-otaku-anime-mujer-kawaii.png`)
       .setThumbnail('https://i.pinimg.com/originals/cc/de/aa/ccdeaa0aa1a833a352d5f5194181c95e.jpg')
-      .setFooter('Codigo Oficial de VhCompany#9999/VhCloud')
+      .setFooter('Codigo Oficial de VhCompany#8710/VhCloud')
       .setTimestamp();
 
   message.channel.send(HelpEmbed)
